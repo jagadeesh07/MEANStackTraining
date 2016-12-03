69bc467ccdaa9089a9331c66fe1cb8761ad0b195
nandyjaga <nandy.jaga@gmail.com>
2016-12-04 00:34:57 +0530

    Angular session 2

--- webapp/app.js
+++ webapp/app.js
@@ -12,6 +12,11 @@ MEANStack.config(function($routeProvider){
 		controller: 'AboutController',
         controllerAs: 'about'
     })
+	.when('/todo', {
+        templateUrl: 'templates/todo.html',
+		controller: 'TodoCtrl',
+        controllerAs: 'TodoCtrl'
+    })
 	.otherwise({
         redirectTo: '/home'
     });
@@ -34,6 +39,47 @@ MEANStack.controller('AboutController',function(){
 	vm.model={
 	    pageName:"About US."
 	}
+});
+
+MEANStack.controller('TodoCtrl',function(){
+    var vm=this;
+	vm.model={
+	   searchStr:"",
+	   taskList:[
+	      {taskName:"Buy groceries", completed:false  },
+	      {taskName:"Pay electricity bills" , completed:false  },
+	      {taskName:"Pay broadband bill" , completed:false  },
+	      {taskName:"Pay credit card bill" , completed:false  }
+	   ],
+	   taskName:""
+	}
+	
+	vm.completeTheTask=function(taskDetl){
+		taskDetl.completed=true;
+	}
+	
+	vm.createTask=function(){
+	    vm.model.taskList.push({taskName:vm.model.taskName,completed:false})
+		vm.model.taskName="";
+	}
+	
+});
+
+MEANStack.controller('MenuCtrl',function(){
+    var vm=this;
+	vm.model={
+	   menuList:[
+	       {value:'home',label:"Home"},
+	       {value:'about',label:"About"},
+	       {value:'todo',label:"To Do"}
+	   ],
+	   selectedMenu:"home"
+	}
+	
+	vm.selectItem=function(menuItem){
+		vm.model.selectedMenu=menuItem.value;
+	}
+	
 });
 
 
